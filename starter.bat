cf7960227dcaafbac0b81ade127834fe33150f21
rustam-shrestha <shrestharama65@gmail.com>
2026-08-18 08:23:29 +0545

    finishing the wrong starteer bat

--- starter.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start powershell -NoExit -Command "cd backend-node; npm run dev"
-start powershell -NoExit -Command "cd backend-fastapi; .\scripts\run_backend.ps1"
-start powershell -NoExit -Command "cd frontend; npm run dev"
